f24ff37cb1648dff0d5414db214bcd713ddde163
Samuel Montoya <samuelmontoyag@gmail.com>
2016-06-28 06:21:32 -0400

    integracion ruby

---
+++ ruby/api_connect/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiConnect::Application.config.secret_token = '30eb0115d405aba89c80e46d4cb99572f0651065fee6af71a1ec3d54c6d4d5d7a3006d28d94c013542f352b1cc2111d39d1cb32a85416911cfc6684fd171f48f'
