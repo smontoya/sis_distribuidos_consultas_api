f823e10dc99800f866e737f0dc68731802e257bb
Maximiliano Augusto Castañón Araneda <maximi89@gmail.com>
2016-06-29 04:10:04 -0400

    ADD: Se agrega eliminar php

---
+++ php/eliminar.php
@@ -0,0 +1,26 @@
+<?php 
+  include("restclient.php"); 
+  $api = new RestClient(array(
+     'base_url' => "localhost:8080/api/")
+  );
+
+  if(isset($_GET['id']))
+  {
+    $result = $api->delete("usuarios/".$_GET['id']);
+    echo json_decode($result->response)->message;
+  }
+  else
+  {
+    echo "Error al eliminar";
+  }
+ ?>
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="/static/css/bootstrap.min.css">
+  </head>
+  <body>
+    </br>
+    <a href="index.php" class="">Volver al listado</a>
+  </body>
+</html>
