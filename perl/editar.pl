b0289914f03f513a2c4b81a9b898df11463252d1
Samuel Montoya <samuelmontoyag@gmail.com>
2016-07-02 10:42:59 -0400

    FIX: editar usuario ahora recibe parametro

--- perl/editar.pl
+++ perl/editar.pl
@@ -1,30 +1,29 @@
 #!/usr/bin/perl
 use LWP::Simple;
 use JSON::PP;
+use CGI;
+my $query = new CGI;
+my $user_id = $query->param('user_id');
 
-$usuarios = decode_json(get("http://localhost:8080/api/usuarios"));
-
+$usuario= decode_json(get("http://localhost:8080/api/usuarios"."/".$user_id));
 
 print "Content-type: text/html\n\n";
 print "<html><head>\n";
 print "<title>CGI Test</title>\n";
-print "</head>\n";
-print "<body>
+print "</head>\n
+  <body>
 	<form method='POST' name='formulario'>";
+
 print "<table class='table table-bordered'>";
 print "<th>Email</th>";
 print "<th>Nombres</th>";
-print "<th>Apellidos</th>";
-foreach my $item( @$usuarios ) { 
- print "
- 	<tr>
- 		<td> <input type='text' value='$item->{email}' /> </td>
- 		<td> <input type='text' value='$item->{nombres}' /> </td>
- 		<td> <input type='text' value='$item->{apellidos}' /> </td>
-	</tr>
- 	";
+print "<th>Apellidos</th>
+<tr>
+ 		<td> <input type='text' value='$usuario->{email}' name='email' /> </td>
+ 		<td> <input type='text' value='$usuario->{nombres}' value='nombres'/> </td>
+ 		<td> <input type='text' value='$usuario->{apellidos}' value='apellidos'/> </td>
+	</tr>";
  
-}
 print "</table>
 		<input type='submit' value='Actualizar Usuario' /> 
 	</form>";
