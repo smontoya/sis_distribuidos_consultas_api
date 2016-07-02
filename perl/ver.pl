836184007b261f3fcc35b3c18ec0bb918f3a5dd7
Samuel Montoya <samuelmontoyag@gmail.com>
2016-07-02 11:19:24 -0400

    FIX: boton editar en ver

--- perl/ver.pl
+++ perl/ver.pl
@@ -1,14 +1,28 @@
 #!/usr/bin/perl
 use LWP::Simple;
 use JSON::PP;
+use CGI;
+my $query = new CGI;
+my $user_id = $query->param('user_id');
 
-# $usuario = decode_json(get("http://localhost:8080/api/usuarios/123"));
+$usuario= decode_json(get("http://localhost:8080/api/usuarios"."/".$user_id));
 
-print "Content-type: text/html\n\n";
-print "<html><head>\n";
-print "<title>CGI Test</title>\n";
-print "</head>\n";
-print "<body>";
-print "<table class='table table-bordered'>";
-print "<th></th>";
-print "<th>Email</th>";
+print "Content-type: text/html\n
+<html>
+  <head>
+    <title>Editar usuario</title>
+  </head>\n
+  <body>
+	  <table class='table table-bordered'>
+	  	<th>Email</th>
+	  	<th>Nombres</th>
+	  	<th>Apellidos</th>
+	    <tr>
+ 		  <td> $usuario->{email}</td>
+ 		  <td> $usuario->{nombres} </td>
+ 		  <td> $usuario->{apellidos}</td>
+	    </tr>
+      </table>
+      <a href='/editar.pl?user_id=$usuario->{_id}'>Actualizar Usuario</a> 
+  </body>
+</html>";
