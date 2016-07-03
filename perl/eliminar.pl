fe61f788d4f6df64c865eb9ecd90681014292678
maximi89 <maximi89@gmail.com>
2016-07-03 03:54:59 -0400

    ADD PERL: pruebas para funcionamiento de eliminar

--- perl/eliminar.pl
+++ perl/eliminar.pl
@@ -1,38 +1,34 @@
 #!/usr/bin/perl
 use LWP::Simple;
 use JSON::PP;
+use HTTP::Request;
+use CGI;
+my $query = new CGI;
+my $user_id = $query->param('user_id');
 
 use LWP::UserAgent;
 my $ua = LWP::UserAgent->new;
-my $servidor = "http://localhost:8080/api/usuarios";
+my $servidor = "http://localhost:8080/api/usuarios/".$user_id;
 
-#$usuarios = decode_json(get("http://localhost:8080/api/usuarios"));
-
-
-print "Content-type: text/html\n\n";
-print "<html><head>\n";
-print "<title>Eliminar Usuario - PERL</title>\n";
-print "</head>\n";
-print "<body>";
-
+print $servidor;
  
 # set custom HTTP request header fields
-my $req = HTTP::Request->new(POST => $servidor);
-$req->header('content-type' => 'application/json');
-$req->header('x-auth-token' => 'kfksj48sdfj4jd9d');
- 
+my $req = HTTP::Request->new(DELETE => $servidor);
+# $req->header('content-type' => 'application/json');
+# $req->header('x-auth-token' => 'kfksj48sdfj4jd9d');
+
 # add POST data to HTTP request body
-my $post_data = '{ "_id": "Dan", "address": "NY" }';
+my $post_data = '{ "_id": "'.$user_id.'" }';
 $req->content($post_data);
  
 my $resp = $ua->request($req);
-if ($resp->is_success) {
-    my $message = $resp->decoded_content;
-    print "Received reply: $message\n";
-}
-else {
-    print "HTTP POST error code: ", $resp->code, "\n";
-    print "HTTP POST error message: ", $resp->message, "\n";
-}
+print $resp->message;
 
-print "</body>";
+# if ($resp->is_success) {
+#     my $message = $resp->decoded_content;
+#     print "Received reply: $message\n";
+# }
+# else {
+#     print "HTTP POST error code: ", $resp->code, "\n";
+#     print "HTTP POST error message: ", $resp->message, "\n";
+# }
